50781e24068a34c52990b0657dcf110f01cab430
Dmitry Adonin <dmitrii.adonin@gmail.com>
2017-10-23 10:09:14 +0300

    Lecture 2, hometask 5.1

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
